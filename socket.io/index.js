ec7e7399d27c5f42f504deb45cc70533a8fc663d
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-07-20 01:33:46 +0300

    Update index.js

--- socket.io/index.js
+++ socket.io/index.js
@@ -8,15 +8,15 @@ let clients = [];
 let status = ['justEnter', 'online', 'offline'];
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/script.js', function(req, res) {
-    res.sendFile(__dirname + '/public/script.js');
+    res.sendFile(__dirname + '/script.js');
 });
 
 app.get('/style.css', function(req, res) {
-    res.sendFile(__dirname + '/public/style.css');
+    res.sendFile(__dirname + '/style.css');
 });
 
 io.on('connection', function(socket) {
